d8f2c44cb8bebdf90c28609a2993eff78868cd5b
Dallas3 <dallasgordon3@gmail.com>
2026-02-10 16:11:24 -0800

    Tuning order updated and clean up

--- src/data/tunings.js
+++ src/data/tunings.js
@@ -8,25 +8,22 @@ export const STANDARD_TUNINGS = {
     },
     '5-string': {
       'standard': ['B0', 'E1', 'A1', 'D2', 'G2'],
-      'drop-a': ['A0', 'E1', 'A1', 'D2', 'G2'],
-      'half-step-down': ['A#0', 'D#1', 'G#1', 'C#2', 'F#2']
+      'half-step-down': ['A#0', 'D#1', 'G#1', 'C#2', 'F#2'],
+      'drop-a': ['A0', 'E1', 'A1', 'D2', 'G2']
     },
     '6-string': {
       'standard': ['B0', 'E1', 'A1', 'D2', 'G2', 'C3'],
       'drop-a': ['A0', 'E1', 'A1', 'D2', 'G2', 'C3']
-    },
-    // '7-string': {
-    //   'standard': ['F#0', 'B0', 'E1', 'A1', 'D2', 'G2', 'C3']
-    // }
+    }
   },
   'electric-guitar': {
     '6-string': {
       'standard': ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
-      'drop-d': ['D2', 'A2', 'D3', 'G3', 'B3', 'E4'],
       'half-step-down': ['D#2', 'G#2', 'C#3', 'F#3', 'A#3', 'D#4'],
       'whole-step-down': ['D2', 'G2', 'C3', 'F3', 'A3', 'D4'],
-      'drop-c': ['C2', 'G2', 'C3', 'F3', 'A3', 'D4'],
+      'drop-d': ['D2', 'A2', 'D3', 'G3', 'B3', 'E4'],
       'drop-c#': ['C#2', 'G#2', 'C#3', 'F#3', 'A#3', 'D#4'],
+      'drop-c': ['C2', 'G2', 'C3', 'F3', 'A3', 'D4'],
       'drop-b': ['B1', 'F#2', 'B2', 'E3', 'G#3', 'C#4'],
       'DADGAD': ['D2', 'A2', 'D3', 'G3', 'A3', 'D4'],
       'open-g': ['D2', 'G2', 'D3', 'G3', 'B3', 'D4'],
@@ -35,9 +32,9 @@ export const STANDARD_TUNINGS = {
     },
     '7-string': {
       'standard': ['B1', 'E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
-      'drop-a': ['A1', 'E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
       'half-step-down': ['A#1', 'D#2', 'G#2', 'C#3', 'F#3', 'A#3', 'D#4'],
       'whole-step-down': ['A1', 'D2', 'G2', 'C3', 'F3', 'A3', 'D4'],
+      'drop-a': ['A1', 'E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
     },
     '8-string': {
       'standard': ['F#1', 'B1', 'E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
@@ -47,20 +44,14 @@ export const STANDARD_TUNINGS = {
   'acoustic-guitar': {
     '6-string': {
       'standard': ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
-      'drop-d': ['D2', 'A2', 'D3', 'G3', 'B3', 'E4'],
       'half-step-down': ['D#2', 'G#2', 'C#3', 'F#3', 'A#3', 'D#4'],
       'DADGAD': ['D2', 'A2', 'D3', 'G3', 'A3', 'D4'],
+      'drop-d': ['D2', 'A2', 'D3', 'G3', 'B3', 'E4'],
       'open-c': ['C2', 'G2', 'C3', 'G3', 'C4', 'E4'],
       'open-d': ['D2', 'A2', 'D3', 'F#3', 'A3', 'D4'],
       'open-e': ['E2', 'B2', 'E3', 'G#3', 'B3', 'E4'],
       'open-g': ['D2', 'G2', 'D3', 'G3', 'B3', 'D4']
-    },
-    // '7-string': {
-    //   'standard': ['B1', 'E2', 'A2', 'D3', 'G3', 'B3', 'E4']
-    // },
-    // '8-string': {
-    //   'standard': ['F#1', 'B1', 'E2', 'A2', 'D3', 'G3', 'B3', 'E4']
-    // }
+    }
   },
   'pure-tone': {
     'ukelele': {
@@ -74,12 +65,6 @@ export const STANDARD_TUNINGS = {
     },
     '6-string guitar': {
       'standard': ['E2', 'A2', 'D3', 'G3', 'B3', 'E4']
-    },
-    // '7-string guitar': {
-    //   'standard': ['B1', 'E2', 'A2', 'D3', 'G3', 'B3', 'E4']
-    // },
-    // '8-string guitar': {
-    //   'standard': ['F#1', 'B1', 'E2', 'A2', 'D3', 'G3', 'B3', 'E4']
-    // }
+    }
   }
 }
